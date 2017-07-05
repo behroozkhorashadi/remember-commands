3571ad36f1362e1b8e0671851d2d0db2858f2ae5
Behrooz Khorashadi <behrooz@uber.com>
2017-07-05 12:56:23 -0700

    Learn it start.

---
+++ LearnIt/gui/src/main/java/com/khorashadi/LearnIt.java
@@ -0,0 +1,74 @@
+package com.khorashadi;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
+import javafx.stage.Stage;
+
+public class LearnIt extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+//        primaryStage.setTitle("Hello World!");
+//        Button btn = new Button();
+//        btn.setText("Say 'Hello World'");
+//        btn.setOnAction(new EventHandler<ActionEvent>() {
+//
+//            @Override
+//            public void handle(ActionEvent event) {
+//                System.out.println("Hello World!");
+//            }
+//        });
+//
+//        StackPane root = new StackPane();
+//        root.getChildren().add(btn);
+//        primaryStage.setScene(new Scene(root, 300, 250));
+//        primaryStage.show();
+        //Creating a line object
+        //Creating a Path
+        Path path = new Path();
+
+        //Moving to the starting point
+        MoveTo moveTo = new MoveTo(108, 71);
+
+        //Creating 1st line
+        LineTo line1 = new LineTo(321, 161);
+
+        //Creating 2nd line
+        LineTo line2 = new LineTo(126,232);
+
+        //Creating 3rd line
+        LineTo line3 = new LineTo(232,52);
+
+        //Creating 4th line
+        LineTo line4 = new LineTo(269, 250);
+
+        //Creating 4th line
+        LineTo line5 = new LineTo(108, 71);
+
+        //Adding all the elements to the path
+        path.getElements().add(moveTo);
+        path.getElements().addAll(line1, line2, line3, line4, line5);
+
+        //Creating a Group object
+        Group root = new Group(path);
+
+        //Creating a scene object
+        Scene scene = new Scene(root, 600, 300);
+
+        //Setting title to the Stage
+        stage.setTitle("Drawing an arc through a path");
+
+        //Adding scene to the stage
+        stage.setScene(scene);
+
+        //Displaying the contents of the stage
+        stage.show();
+    }
+}

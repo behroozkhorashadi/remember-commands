54a471e9dedaeacd93a16934a04d2b1348905567
Behrooz Khorashadi <beh@khorashadi.com>
2017-08-31 14:19:34 -0700

    Added tests for the interactive module.

--- remember/interactive.py
+++ remember/interactive.py
@@ -13,12 +13,14 @@ class InteractiveCommandExecutor(object):
         self._history_file_path = history_file_path
 
     def run(self, result):
+        """Interactively enumerate a set of commands and pick one to run."""
         self._enumerate_commands(result)
         return self._select_command(result)
 
     def _enumerate_commands(self, command_results):
         for idx, command in enumerate(command_results):
-            print (bcolors.HEADER + "(" + str(idx + 1) + ") " + bcolors.OKGREEN + command.get_unique_command_id()
+            print (bcolors.HEADER + "(" + str(idx + 1) + ") "
+                   + bcolors.OKGREEN + command.get_unique_command_id()
                    + bcolors.ENDC)
 
     def _select_command(self, command_results):
@@ -36,6 +38,7 @@ class InteractiveCommandExecutor(object):
             return False
 
     def set_command_info(self, command_results):
+        """Interactively choose a command to set command info for."""
         self._enumerate_commands(command_results)
 
         user_input = get_user_input('Choose command by # or ' +
@@ -52,6 +55,7 @@ class InteractiveCommandExecutor(object):
             return False
 
     def delete_interaction(self, store, commands):
+        """Delete a command from the store."""
         changes_made = False
         ask = True
         for command in commands:
--- remember/interactive_unittest.py
+++ remember/interactive_unittest.py
@@ -1,16 +1,18 @@
 #! /usr/bin/env python
 #pylint: skip-file
+import subprocess
 import unittest
 
+import command_store_lib
 import interactive
 from command_store_lib import Command
 from interactive import InteractiveCommandExecutor
+from functools import partial
 
 
 class Test(unittest.TestCase):
 
-    # get_input will return 'yes' during this test
-    def test_answer_yes(self):
+    def test_command_update_info_should_correctly_set_info(self):
         interactive = InteractiveCommandExecutor()
         command = Command("git rest --hard HEAD")
         command_info = 'command info'
@@ -19,6 +21,90 @@ class Test(unittest.TestCase):
         self.assertEqual(command.get_command_info(), command_info)
         self.reset_input()
 
+    def test_command_update_info_should_fail_set_info_becuase_exit(self):
+        interactive = InteractiveCommandExecutor()
+        command = Command("git rest --hard HEAD")
+        self.set_input('exit')
+        self.assertFalse(interactive.set_command_info([command]))
+        self.assertEqual(command.get_command_info(), "")
+        self.reset_input()
+
+    def test_delete_command_from_store_should_delete(self):
+        store = command_store_lib.CommandStore()
+        command = Command("testing delete this command")
+        store.add_command(command)
+        self.assertEqual(store.get_num_commands(), 1)
+        interactive = InteractiveCommandExecutor()
+        self.set_input('y')
+        self.assertTrue(interactive.delete_interaction(store, [command]))
+        self.assertEqual(store.get_num_commands(), 0)
+        self.reset_input()
+
+    def test_delete_command_from_store_should_not_delete(self):
+        store = command_store_lib.CommandStore()
+        command = Command("testing delete this command")
+        store.add_command(command)
+        self.assertEqual(store.get_num_commands(), 1)
+        interactive = InteractiveCommandExecutor()
+        self.set_input('n')
+        self.assertFalse(interactive.delete_interaction(store, [command]))
+        self.assertEqual(store.get_num_commands(), 1)
+        self.reset_input()
+
+    def test_delete_command_from_store_random_input_should_not_delete(self):
+        store = command_store_lib.CommandStore()
+        command = Command("testing delete this command")
+        store.add_command(command)
+        self.assertEqual(store.get_num_commands(), 1)
+        interactive = InteractiveCommandExecutor()
+        self.set_input('random anything')
+        self.assertFalse(interactive.delete_interaction(store, [command]))
+        self.assertEqual(store.get_num_commands(), 1)
+        self.reset_input()
+
+    def test_delete_command_from_store_with_exit_input_should_not_delete(self):
+        store = command_store_lib.CommandStore()
+        command = Command("testing delete this command")
+        store.add_command(command)
+        self.assertEqual(store.get_num_commands(), 1)
+        interactive = InteractiveCommandExecutor()
+        self.set_input('exit')
+        self.assertFalse(interactive.delete_interaction(store, [command]))
+        self.assertEqual(store.get_num_commands(), 1)
+        self.reset_input()
+
+    def test_delete_command_from_store_with_all_should_remove_all(self):
+        store = command_store_lib.CommandStore()
+        command = Command("testing delete this command")
+        command2 = Command("remove this also")
+        self.assertEqual(store.get_num_commands(), 0)
+        store.add_command(command)
+        store.add_command(command2)
+        self.assertEqual(store.get_num_commands(), 2)
+        interactive = InteractiveCommandExecutor()
+        self.set_input('allofthem')
+        self.assertTrue(interactive.delete_interaction(store, [command, command2]))
+        self.assertEqual(store.get_num_commands(), 0)
+        self.reset_input()
+
+    def test_run_when_command_is_executed(self):
+        old_call = subprocess.call
+        store = command_store_lib.CommandStore()
+        command_str = "testing delete this command"
+        command = Command(command_str)
+        command2 = Command("remove this also")
+        test_call = partial(self.subprocess_call_mock, expected=command_str)
+        subprocess.call = test_call
+        self.assertEqual(store.get_num_commands(), 0)
+        store.add_command(command)
+        store.add_command(command2)
+        self.assertEqual(store.get_num_commands(), 2)
+        interactive = InteractiveCommandExecutor()
+        self.set_input('1')
+        self.assertTrue(interactive.run([command, command2]))
+        self.reset_input()
+        subprocess.call = old_call
+
     def set_input(self,  *args):
         self.original_raw_input = interactive.get_user_input
         interactive.get_user_input =  InputMock(args)
@@ -26,6 +112,10 @@ class Test(unittest.TestCase):
     def reset_input(self):
         if self.original_raw_input:
             interactive.get_user_input = self.original_raw_input
+
+    def subprocess_call_mock(self, command_str, expected, shell):
+        self.assertEqual(expected, command_str)
+        self.assertTrue(shell)
 
 class InputMock( object ):
     def __init__(self, args):

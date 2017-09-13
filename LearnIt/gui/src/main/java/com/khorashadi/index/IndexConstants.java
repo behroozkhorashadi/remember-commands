67d6f85e0c6d0c5a082144e722afbac6c76d28d1
Behrooz Khorashadi <beh@khorashadi.com>
2017-09-13 11:52:37 -0700

    Removed the java stuff and just left the command line stuff.

--- LearnIt/gui/src/main/java/com/khorashadi/index/IndexConstants.java
+++
@@ -1,9 +0,0 @@
-package com.khorashadi.index;
-
-public final class IndexConstants {
-    private IndexConstants() { }
-
-    public static final String TAGS = "tags";
-    public static final String CONTENTS = "contents";
-    public static final String UUID = "filename";
-}
